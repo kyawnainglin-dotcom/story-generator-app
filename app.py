96563f8be7346bbd6cd51d7458f7689de24a8c95
kyawnainglin <123849566+kyawnainglin-dotcom@users.noreply.github.com>
2026-06-17 00:41:54 +0800

    Update app.py

--- app.py
+++ app.py
@@ -167,7 +167,7 @@ if st.button("Generate"):
             }
             
             model = genai.GenerativeModel(
-                model_name='gemini-1.5-flash',
+                model_name='gemini-2.5-flash',
                 generation_config=generation_config
             )
             
