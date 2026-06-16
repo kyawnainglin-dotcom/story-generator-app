e16b3b4c1d0777a3a18615c07a8534f5803330fa
kyawnainglin <123849566+kyawnainglin-dotcom@users.noreply.github.com>
2026-06-17 00:10:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.markdown("""
     <style>
         /* 1. Raw Background Photo (NO MASK) - High Clarity */
         .stApp {
-            background-image: url('https://pixabay.com/images/download/x-9295186_1920.jpg');
+            background-image: url('https://pixabay.com/images/download/tieubaotruong-field-9295186_1920.jpg');
             background-size: cover;
             background-position: center;
             background-attachment: fixed;
